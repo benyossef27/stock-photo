2dcda4979715e6d0ad557811ea9cf5e94b91c566
benyossef27 <93250111+benyossef27@users.noreply.github.com>
2022-08-30 00:34:23 +0300

    Updates

---
+++ precache-manifest.804868bb99bfb1c76c5744f597e93bc1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10b8d84b03f6963c374c550346f29f1c",
+    "url": "/stock-photo/index.html"
+  },
+  {
+    "revision": "d1921861ea5d58054870",
+    "url": "/stock-photo/static/css/main.59924b59.chunk.css"
+  },
+  {
+    "revision": "b5a99b81e43d98e30c35",
+    "url": "/stock-photo/static/js/2.ee72bb2a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/stock-photo/static/js/2.ee72bb2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1921861ea5d58054870",
+    "url": "/stock-photo/static/js/main.3cc1f583.chunk.js"
+  },
+  {
+    "revision": "27ac3c4e125aef47bc24",
+    "url": "/stock-photo/static/js/runtime-main.db0f4ddb.js"
+  }
+]);
